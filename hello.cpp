7581418ea404e24b7b66c993ea896afc8c8cd06a
danilin-09 <daniellelindsay48@gmail.com>
2026-02-03 21:26:28 +0000

    first practice

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+//skeleton for C++ programs
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main (){
+    cout << "Hello World ";
+    cout << "\n";
+    cout << "My name is " <<endl;
+    cout << "danielle \n";
+    return 0;
+}
