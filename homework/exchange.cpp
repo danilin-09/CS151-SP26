2e2948fad7ca30134dde322b69aa22cb3d555e19
danilin-09 <daniellelindsay48@gmail.com>
2026-03-14 16:53:08 +0000

    not work

--- homework/exchange.cpp
+++ homework/exchange.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
 //danielle lindsay
 using namespace std;
 
@@ -8,6 +9,14 @@ int main(){
     double yenPerDollar = 98.93;  //conversion rates
     double euroPerDollar = .74;
 
+    double dollars;  //user input
+    cout << "Please enter the amount of USD you would like to convert: \n";
+    cin >> dollars;  //input in dollars and cents
+
+    double yenTotal = dollars * yenPerDollar;
+    double eurosTotal = dollars * euroPerDollar;
+
+    cout << "$" << dollars << " is equivalent to " << setprecision(4) << eurosTotal << "euros or " << yenTotal << " yen. \n";
 
     return 0;
 }
